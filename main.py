237549584ecd8772ac4a2c9a597f7e646f9a3732
WangYun <294961525@qq.com>
2024-12-15 20:33:11 +0800

    241215: update main prompt

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 from dotenv import load_dotenv, find_dotenv
 from langchain_openai import ChatOpenAI
-from langchain.prompts import PromptTemplate
-from langchain_core.runnables import RunnablePassthrough
-from langchain.schema import StrOutputParser
 
 
 def __init_main_prompt():
     with open("main_prompt.txt", "r", encoding="utf-8") as f:
-        main_prompt = PromptTemplate.from_template(f.read())
-    return main_prompt
+        return f.read()
 
 
-def __init_chian(main_prompt: PromptTemplate, llm: ChatOpenAI):
-    return {"docs": RunnablePassthrough()} | main_prompt | llm | StrOutputParser()
+def __save_cases(ret: str):
+    with open("cases.md", "a", encoding="utf-8") as f:
+        f.write(ret)
 
 
 if __name__ == "__main__":
@@ -20,8 +17,6 @@ if __name__ == "__main__":
 
     llm = ChatOpenAI(model="gpt-4o-mini")
     main_prompt = __init_main_prompt()
-    chain = __init_chian(main_prompt, llm)
 
-    docs = input("请提供您的需求文档：\n")
-    for s in chain.stream(docs):
-        print(s, end="", flush=True)
+    ret = llm.invoke(main_prompt)
+    __save_cases(ret.content)
